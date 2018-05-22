9b9e698b30269b3ee5981ebd4fcd27cdcf107e17
Jakub Gemrot <jakub.gemrot@gmail.com>
2018-05-22 17:05:29 +0200

    NavMeshDebugBot: adding synth-testing of the path-finding, the bot now can iterate path-finding requests through almost all (discretized) positions within the map (just path-finding, not navigation)

--- Main/PogamutUT2004Examples/07-NavMeshDebugBot/src/main/java/cz/cuni/amis/pogamut/ut2004/examples/navmeshdebugbot/NavMeshDebugBot.java
+++ Main/PogamutUT2004Examples/07-NavMeshDebugBot/src/main/java/cz/cuni/amis/pogamut/ut2004/examples/navmeshdebugbot/NavMeshDebugBot.java
@@ -1,21 +1,30 @@
 package cz.cuni.amis.pogamut.ut2004.examples.navmeshdebugbot;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
+import javax.vecmath.Vector3d;
+
+import cz.cuni.amis.pogamut.base.agent.navigation.impl.PrecomputedPathFuture;
 import cz.cuni.amis.pogamut.base.communication.worldview.listener.annotation.EventListener;
 import cz.cuni.amis.pogamut.base.utils.guice.AgentScoped;
+import cz.cuni.amis.pogamut.base3d.worldview.object.Location;
 import cz.cuni.amis.pogamut.ut2004.agent.module.utils.UT2004Skins;
 import cz.cuni.amis.pogamut.ut2004.agent.navigation.navmesh.drawing.INavMeshDraw;
-import cz.cuni.amis.pogamut.ut2004.agent.navigation.navmesh.drawing.NavMeshDraw;
+import cz.cuni.amis.pogamut.ut2004.agent.navigation.navmesh.pathPlanner.polygonPathFunnel.FunnelDebug;
 import cz.cuni.amis.pogamut.ut2004.bot.impl.UT2004Bot;
 import cz.cuni.amis.pogamut.ut2004.bot.impl.UT2004BotModuleController;
 import cz.cuni.amis.pogamut.ut2004.communication.messages.gbcommands.Initialize;
 import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.GlobalChat;
 import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.NavPoint;
 import cz.cuni.amis.pogamut.ut2004.utils.UT2004BotRunner;
+import cz.cuni.amis.utils.ExceptionToString;
 import cz.cuni.amis.utils.IFilter;
+import cz.cuni.amis.utils.Tuple2;
 import cz.cuni.amis.utils.collections.MyCollections;
 import cz.cuni.amis.utils.exception.PogamutException;
 
@@ -37,7 +46,7 @@ public class NavMeshDebugBot extends UT2004BotModuleController {
 	private static int INSTANCE = 0;
 	
     private long   lastLogicTime        = -1;
-    private long   logicIterationNumber = 0;
+    private long   logicIterationNumber =  0;
     
     private INavMeshDraw navMeshDraw;
     
@@ -64,7 +73,12 @@ public class NavMeshDebugBot extends UT2004BotModuleController {
     /**
      * Flip by chat message 'drawNavMesh'.
      */
-    private boolean drawNavMesh = true;
+    private boolean drawNavMesh = false;
+    
+    /**
+     * Perform syntehetic test from the start ... probing many paths, stress-testing navmesh path-finder...
+     */
+    private boolean synthTest = true;
     
     /**
      * Navpoint we are running to.
@@ -128,10 +142,15 @@ public class NavMeshDebugBot extends UT2004BotModuleController {
     	if (msg.getText().toLowerCase().startsWith("clear")) {
     		draw.clearAll();
     		sayGlobal("ALL CLEARED!");
-    	}    
+    	} else 
+		if (msg.getText().toLowerCase().startsWith("synthtest")) {
+    		draw.clearAll();
+    		navigation.stopNavigation();
+    		pathFindingTest();
+    	}
     }
     
-    @Override
+	@Override
     public void beforeFirstLogic() {
     	sayGlobal("AUTO NAVIGATION: " + auto);
     	sayGlobal("NAVIGATE ONLY ITEMS: " + navigateItems);
@@ -150,6 +169,12 @@ public class NavMeshDebugBot extends UT2004BotModuleController {
     	if (!navMeshModule.isInitialized()) {
     		sayGlobal("NAV MESH MODULE NOT INITIALIZED?");
     		sayGlobal("Missing navmesh for: " + game.getMapName() + " ?");
+    		return;
+    	}
+    	
+    	if (synthTest) {
+    		synthTest = false;
+    		pathFindingTest();
     		return;
     	}
     	
@@ -200,6 +225,132 @@ public class NavMeshDebugBot extends UT2004BotModuleController {
     	}
 
     }
+    
+    private void pathFindingTest() {
+		sayGlobal("SYNTH TESTING! Hanging up the bot logic... see logs.");
+		
+    	log.warning("===========================");
+		log.warning("PATH-FINDING STRESS TESTING");
+		log.warning("===========================");
+		
+		log.warning("  +-- map: " + game.getMapName());	
+		log.warning("  +-- computing extense...");
+		Vector3d mins = new Vector3d(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+		Vector3d maxs = new Vector3d(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
+				
+		for (NavPoint np : navPoints.getNavPoints().values()) {
+			if (np.getLocation().getX() > maxs.x) maxs.x = np.getLocation().getX();
+			if (np.getLocation().getY() > maxs.y) maxs.y = np.getLocation().getY();
+			if (np.getLocation().getZ() > maxs.z) maxs.z = np.getLocation().getZ();
+			
+			if (np.getLocation().getX() < mins.x) mins.x = np.getLocation().getX();
+			if (np.getLocation().getY() < mins.y) mins.y = np.getLocation().getY();
+			if (np.getLocation().getZ() < mins.z) mins.z = np.getLocation().getZ();			
+		}
+		
+		int randomSeed = 1;
+		double step = 31;
+		
+		log.warning("  +-- CUBE[ " + mins + " - " + maxs + "]");		
+		log.warning("  +-- TESTING! Step is: " + step + "; random seed is " + randomSeed);
+		int extenseX = (int)Math.floor((maxs.x - mins.x) / step);
+		int extenseY = (int)Math.floor((maxs.y - mins.y) / step);
+		int extenseZ = (int)Math.floor((maxs.z - mins.z) / step);
+		log.warning("  +-- Start locations (2D)    about: " + extenseX + "*" + extenseY + " = " + (extenseX*extenseY));
+		log.warning("  +-- Start locations (total) about: " + extenseX + "*" + extenseY + "*" + extenseZ + " = " + (extenseX*extenseY*extenseZ));
+		
+		Random random = new Random(randomSeed);
+		
+		int tests = 0;
+		int fails = 0;
+		int notExists = 0;
+		
+		List<Tuple2<Location, Location>> failures = new ArrayList<Tuple2<Location, Location>>();		
+		
+		for (double x = mins.x; x < maxs.x + step; x += step) {
+			for (double y = mins.y; y < maxs.y + step; y += step) {
+				for (double z = mins.z; z < maxs.z + step; z += step) {
+					// CHECK START
+					Location start = new Location(x, y, z);
+					if (navMeshModule.getDropGrounder().tryGround(start) == null) {
+						// INVALID LOCATION
+						continue;						
+					}
+					
+					// FIND RANDOM TARGET
+					Location target = null;
+					while (target == null || navMeshModule.getDropGrounder().tryGround(target) == null) {
+						target = new Location(random.nextDouble() * (maxs.x - mins.x) + mins.x, random.nextDouble() * (maxs.y - mins.y) + mins.y, random.nextDouble() * (maxs.z - mins.z) + mins.z);
+					}
+					
+					// PERFORM THE PATH-FINDING
+					log.info("  +-- Path-finding: " + start + " -> " + target + " ...");
+					
+					++tests;
+					
+					try {
+						PrecomputedPathFuture pf = navMeshModule.getAStarPathPlanner().computePath(start, target);
+						if (pf.get() == null) {
+							log.info("    +-- NOT EXISTS!");
+							++notExists;
+						} else {
+							log.info("    +-- Path-length " + pf.get().size());
+						}
+					} catch (Exception e) {
+						log.info(ExceptionToString.process("AT " + game.getMapName() + " FAILED TO COMPUTE THE PATH: " + start + " -> " + target, e));
+						++fails;
+						failures.add(new Tuple2<Location, Location>(start, target));
+					}
+				}	
+			}				
+		}
+		
+		for (NavPoint np1 : navPoints.getNavPoints().values()) {
+			for (NavPoint np2 : navPoints.getNavPoints().values()) {
+				log.info("  +-- Path-finding: " + np1.getId().getStringId() + " -> " + np2.getId().getStringId() + " ...");
+				
+				++tests;
+				
+				try {
+					PrecomputedPathFuture pf = navMeshModule.getAStarPathPlanner().computePath(np1, np2);
+					if (pf.get() == null) {
+						log.info("    +-- NOT EXISTS!");
+						++notExists;
+					} else {
+						log.info("    +-- Path-length " + pf.get().size());
+					}
+				} catch (Exception e) {
+					log.info(ExceptionToString.process("AT " + game.getMapName() + " FAILED TO COMPUTE THE PATH: " + np1.getId().getStringId() + " -> " + np2.getId().getStringId(), e));
+					++fails;
+					failures.add(new Tuple2<Location, Location>(np1.getLocation(), np2.getLocation()));
+				}
+			}
+		}
+		
+		log.warning("RESULT for " + game.getMapName() + ": " + (tests - fails) + " / " + tests + " SUCCEEDED, " + notExists + " / " + tests + " paths not-exist");
+		
+		if (fails > 0) {
+			if (fails == 1) {
+				log.warning("THERE WAS 1 FAILURE!");
+			}else {
+				log.warning("THERE WERE " + fails + " FAILURES!");
+			}
+			for (Tuple2<Location, Location> failure : failures) {
+				log.warning("  +-- FAILED TO COMPUTE THE PATH: " + failure.getFirst() + " -> " + failure.getSecond());
+				
+				// DEBUG HOOK
+				FunnelDebug.debug = true;
+				FunnelDebug.draw = draw;				
+				
+				// PUT A BREAKPOINT HERE TO RUN THE PATH-FINDING AGAIN USING BREAKPOINTS...
+				Location start = failure.getFirst();
+				Location target = failure.getSecond();
+				navMeshModule.getAStarPathPlanner().computePath(start, target);				
+			}
+			throw new RuntimeException("PATH-FINDING IS FAILING FOR THIS MAP !!!");
+		}
+    	
+	}
 
     /**
      * This method is called when the bot is started either from IDE or from
